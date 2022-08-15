5ce9d962f9a2ac4add8b6dbd4d3ccc23ede3388a
AlejandroM12 <alejandro.morel@gmail.com>
2022-08-15 18:39:14 -0300

    Added category component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory"
 
 
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState('')
+
+    const onAddCategory = (newCategory) => {
+        if( categories.includes(newCategory) ) return;
+
+        setCategories([newCategory, ...categories])
+    }
   return (
     <>
     <h1>GifExpertApp</h1>
+    <AddCategory onNewCategory={ (value) => onAddCategory(value) }/>
     </>
   )
 }
