13daafe7c6296b64b7e29ddcd39a4825225ae7d5
AlejandroM12 <alejandro.morel@gmail.com>
2022-08-15 21:54:01 -0300

    Added custom hook

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -2,7 +2,7 @@ export const getGifs = async( category ) => {
     const url = `https://api.giphy.com/v1/gifs/search?api_key=fsvJSPhUD4mM2swGQkA7Jvqte6w33lfD=${ category }&limit=20`;
     const resp = await fetch( url );
     const { data } = await resp.json();
-
+    
     const gifs = data.map( img => ({
         id: img.id,
         title: img.title,
